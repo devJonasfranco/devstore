26e419b6281fc27af9d094396dac6ae7972c5267
devJonasfranco <minhacontajonas@gmail.com>
2024-07-02 20:28:01 -0300

    adicionado valor no produtos e subprodutos

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         href="/"
-        className=" group flex item-end justify-center col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
+        className=" relative group flex item-end justify-center col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
@@ -16,10 +16,16 @@ export default function Home() {
           quality={100}
           alt=""
         />
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$120,00
+          </span>
+        </div>
       </Link>
       <Link
         href="/"
-        className=" group flex item-end justify-center col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
+        className=" relative group flex item-end justify-center col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
@@ -29,10 +35,16 @@ export default function Home() {
           quality={100}
           alt=""
         />
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$120,00
+          </span>
+        </div>
       </Link>
       <Link
         href="/"
-        className=" group flex item-end justify-center col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
+        className="relative group flex item-end justify-center col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
@@ -42,6 +54,12 @@ export default function Home() {
           quality={100}
           alt=""
         />
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$120,00
+          </span>
+        </div>
       </Link>
     </div>
   )
