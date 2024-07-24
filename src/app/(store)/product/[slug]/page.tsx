b29b9ed8650b4b328dd5851feaa35752d6e824ef
devJonasfranco <minhacontajonas@gmail.com>
2024-07-24 11:01:12 -0300

    adicionado função para geração de pagina estatica na cache e dando build

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -30,6 +30,15 @@ export async function generateMetadata({
   }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((state) => {
+    return { slug: state.slug }
+  })
+}
+
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
 
