98f23111256917a345e33bd21412db293f8880cc
devJonasfranco <minhacontajonas@gmail.com>
2024-07-24 10:04:24 -0300

    criando route.ts no product para cada produto uma rota

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,5 +1,7 @@
 import data from '../../products/data.json'
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   const featuredProducts = data.products.filter((products) => products.featured)
   return Response.json(featuredProducts)
 }
