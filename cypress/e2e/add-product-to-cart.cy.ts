36bb71c1c2e6f5d69dd7d9866b281b3e7ac15d49
devJonasfranco <minhacontajonas@gmail.com>
2024-08-02 15:23:28 -0300

    teste realizado com cypress, teste feitos para search, e navegação, e add product no card.

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,6 +1,9 @@
 describe('add prodcut to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
   it('should be able to navigate to the product page and it to the cart', () => {
-    cy.visit('http://localhost:3000/')
     cy.get('a[href^="/product"]').first().click()
 
     // cy.url().should('include', '/product')
@@ -10,7 +13,6 @@ describe('add prodcut to cart', () => {
     cy.contains('Cart (1)').should('exist')
   })
   it('should not count duplicated product on cart', () => {
-    cy.visit('http://localhost:3000/')
     cy.get('a[href^="/product"]').first().click()
 
     // cy.url().should('include', '/product')
@@ -21,8 +23,6 @@ describe('add prodcut to cart', () => {
     cy.contains('Cart (1)').should('exist')
   })
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
-
     cy.get('input[name=q]').type('moletom').parent('form').submit()
 
     cy.get('a[href^="/product"]').first().click()
